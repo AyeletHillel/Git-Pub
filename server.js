0f56587999184098b308339e33dd36de92c50d8f
Ayelet Hillel <ah3708@tc.columbia.edu>
2022-11-13 18:39:26 -0500

    render drinks on page

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ const app = express()
 require('dotenv').config()
 const PORT = 3000
 
+const drinks = require('./models/drinks.js');
+
+
 app.get("/", (req, res) => {
     res.send('Welcome to the Gitpub App!')
 })
+
+// app.get("/drinks", (req, res) => {
+//     res.send(drinks)
+// })
+
+// INDEX ROUTE - GET to /fruits - Returns all fruits
+app.get('/drinks/', (req, res) => {
+    // res.render(template, data)
+    res.render(
+        'drinks_index.ejs',
+        {
+            allDrinks:drinks
+        }
+    );
+});
 
 app.listen(PORT, (req, res) => {
     console.log(`listening to port ${PORT}`)
